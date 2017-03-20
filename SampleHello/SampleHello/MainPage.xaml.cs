ce3d3e6a605b696b5d89dc82623eb492556ad8d3
erick <erick@DESKTOP-JIKBK0G>
2017-03-20 13:54:38 +0700

    Add project files.

---
+++ SampleHello/SampleHello/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SampleHello
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            btnSubmit.Clicked += BtnSubmit_Clicked;
+        }
+
+        private async void BtnSubmit_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Keterangan", string.Format("Nama anda {0} {1}",
+               txtFirstName.Text, txtLastName.Text), "OK");
+        }
+
+        
+    }
+}
